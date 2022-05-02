9c3daf358df38543568f7b61665929be48d88a10
Yongmin Park <yongmipa@c7r9s2.42seoul.kr>
2022-05-02 16:14:05 +0900

    PS

---
+++ c11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yongmipa <yongmipa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 16:04:37 by yongmipa          #+#    #+#             */
+/*   Updated: 2022/05/02 16:12:31 by yongmipa         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[j] && tab[j + 1])
+		{
+			if (cmp(tab[j], tab[j]) > 0)
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
