1a8572e5f5fb4cb7524980fbb6cb3cc1b3d160d1
Yongmin Park <yongmipa@c7r9s2.42seoul.kr>
2022-05-02 16:04:15 +0900

    A

---
+++ c11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yongmipa <yongmipa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 13:55:48 by yongmipa          #+#    #+#             */
+/*   Updated: 2022/05/02 16:04:07 by yongmipa         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_cmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[j] && tab[j + 1])
+		{
+			if (ft_cmp(tab[j], tab[j + 1] > 0))
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
