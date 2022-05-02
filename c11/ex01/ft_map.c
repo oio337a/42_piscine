613ed00614b0cbc8ea05506060c11599124e3ff1
Yongmin Park <yongmipa@c7r9s2.42seoul.kr>
2022-05-02 11:29:23 +0900

    PS

---
+++ c11/ex01/ft_map.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yongmipa <yongmipa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/02 11:20:58 by yongmipa          #+#    #+#             */
+/*   Updated: 2022/05/02 11:27:41 by yongmipa         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	i;
+	int	*answer;
+
+	i = 0;
+	answer = (int *)malloc(sizeof(int) * length);
+	if (!answer)
+		return (0);
+	while (i < length)
+	{
+		answer[i] = f(tab[i]);
+		i++;
+	}
+}
